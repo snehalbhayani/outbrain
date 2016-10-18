88eda9c706c35106fed7ef6986d0fba94f5ea95f
Snehal Bhayani <snehal.bhayani@ignitesol.com>
2016-10-18 19:02:32 +0530

    Next commit on data sanity attempt

--- clickstrain.py
+++ clickstrain.py
@@ -9,10 +9,9 @@ This is a temporary script file.
 import pandas as pd
 import numpy as np
 
-
 DEFAULT_PATH_TO_DATA = 'outbrainclick/'
 def get_store():
-    return pd.HDFStore('database')
+    return pd.HDFStore('database.db')
 
 def close_store(store):
     store.close()
@@ -24,7 +23,13 @@ def put_into_HDFStore(table, chunk_size):
     except Exception:
         pass
     for chunk in pd.read_csv(DEFAULT_PATH_TO_DATA + table+'.csv', chunksize = chunk_size):
-        print(chunk.shape[0])
+        print(chunk['platform'].unique())
+        chunk['platform'] = chunk['platform'].apply(lambda x: 7 if x == '\\N' else x)
+#        chunk[chunk['platform'] == "1"] = 4        
+#        chunk[chunk['platform'] == '2' ] = 5
+#        chunk[chunk['platform'] == '3' ] = 6
+#        chunk[chunk['platform'] == '\\N' ] = 7
+        print(chunk['platform'].unique())
         store.append(table, chunk, data_columns = chunk.columns.values)
     #close the store
     close_store(store)
@@ -33,3 +38,4 @@ def get_data_by_id(table, field, value):
     store = get_store()
     return pd.read_hdf('database', table, where=field+ '='+ value)
     close_store(store)
+    
